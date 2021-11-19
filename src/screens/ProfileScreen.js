030d026b339eec3767d0817a7ed4da6068673383
Baptiste CERDAN <baptiste.owenia@gmail.com>
2021-11-19 11:52:09 +0100

    :bug: fix profile

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -7,7 +7,7 @@ const Profile = () => {
 const Component = () => {
 	return (
 		<Card containerStyle={{ width: '75%', backgroundColor: '#303030', borderColor: '#ff9e00', borderRadius: 12, borderWidth : 2}}>
-			<Avatar style={{alignSelf: 'center', width : 100, height :100 }} rounded source={require('../assets/user.png')} />
+			<Avatar style={{alignSelf: 'center', width : 100, height :100 }} rounded source={require('../assets/mh_icon.png')} />
 			<Card.Title style={{ color: '#ff9e00', fontSize: 32, marginTop: 20}}>Profile</Card.Title>
 			<Card.Divider/> 
 		</Card>
